42dfc62d018178cf3b021c4932fc924d4d83824c
Kevin Spencer <kevin@kevinspencer.org>
2026-04-08 23:13:36 -0400

    initial DB config things

---
+++ api/db.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once __DIR__ . '/../config.php';
+
+function get_pdo(): PDO {
+    static $pdo = null;
+    if ($pdo === null) {
+        $pdo = new PDO(
+            'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME . ';charset=utf8mb4',
+            DB_USER,
+            DB_PASS,
+            [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+        );
+    }
+    return $pdo;
+}
